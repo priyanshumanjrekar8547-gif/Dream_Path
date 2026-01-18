d2de2e30479236735a288e02e621ad47787b4103
PriyanshuManjrekar <priyanshumanjrekar8547@gmail.com>
2026-01-18 06:24:22 +0530

    delete

--- constants.ts
+++
@@ -1,5 +0,0 @@
-
-import { LearnerProfile, LearningType } from './types';
-
-export const LEARNER_PROFILES = Object.values(LearnerProfile);
-export const LEARNING_TYPES = Object.values(LearningType);
