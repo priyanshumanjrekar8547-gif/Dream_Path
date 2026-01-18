d6d9735139779a22a451b59465ab41ca18597cef
PriyanshuManjrekar <priyanshumanjrekar8547@gmail.com>
2026-01-18 06:21:40 +0530

    v2

---
+++ constants.ts
@@ -0,0 +1,5 @@
+
+import { LearnerProfile, LearningType } from './types';
+
+export const LEARNER_PROFILES = Object.values(LearnerProfile);
+export const LEARNING_TYPES = Object.values(LearningType);
